4103be80ceb438738e0624eaeb483654dc25ab04
Andrijrbsh <124566784+Andrijrbsh@users.noreply.github.com>
2024-10-21 17:41:10 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -15,7 +15,7 @@ function move(position, roll) {
 
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet(name, owner) {
-  return name === owner ? "Hello boss" : "Hello guest";
+  return 'Hello ' + (name === owner ? 'boss' : 'guest');
 }
 
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
