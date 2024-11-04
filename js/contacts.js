f211e8f50d19ed83a0bfcdb9665bbcf493c8f11d
Andrijrbsh <124566784+Andrijrbsh@users.noreply.github.com>
2024-11-04 05:55:23 +0000

    Add last part in contacts

--- js/contacts.js
+++ js/contacts.js
@@ -13,4 +13,14 @@ function init() {
     loadedPartialsCount++;
     if (loadedPartialsCount === totalPartials) init();
   });
+
+  function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.style.display === 'none' || section.style.display === '') {
+        section.style.display = 'block';
+    } else {
+        section.style.display = 'none';
+    }
+}
+
   
